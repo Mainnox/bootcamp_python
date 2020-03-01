8a3ee7d6be782d9677ea9b5b84bcc7737a45af94
Arnaud KREMER <akremer@e2r4p11.42.fr>
2020-03-01 09:36:18 +0100

    Poil

---
+++ Day00/ex01/exec.py
@@ -0,0 +1,26 @@
+import sys
+
+string = []
+gnirts = []
+uplo = ""
+count = 0
+for arg in sys.argv:
+    string.append(arg)
+    count +=  1
+string.remove("exec.py")
+for i in string:
+    gnirts.append(i[::-1])
+for i in gnirts:
+    for a in i:
+        if (a.isupper()) == True:
+            uplo += a.lower()
+        elif (a.islower()) == True:
+            uplo += a.upper()
+        else:
+            uplo += a
+    if 2 < count:
+        uplo += ' '
+        count -= 1
+x = " ".join(uplo)
+if count > 1:
+    print uplo
